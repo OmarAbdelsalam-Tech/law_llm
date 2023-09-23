0aecd0976cf99b7bd822046052d7204649b7fc9d
Omar <98291870+OmarAbdelsalam-Tech@users.noreply.github.com>
2023-09-22 21:04:39 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,7 @@
 import streamlit as st
 import os
 import requests
-from transformers import AutoModelForCausalLM, AutoTokenizer, AutoConfig
-
-from transformers import dynamic_module_utils
-
-# Override the function to always return False
-def patched_resolve_trust_remote_code(*args, **kwargs):
-    return False
-
-dynamic_module_utils.resolve_trust_remote_code = patched_resolve_trust_remote_code
-
-
-# Check if 'model_loaded' is in the session state
-if 'model_loaded' not in st.session_state:
-    st.session_state.model_loaded = False
+from transformers import AutoModelForCausalLM, AutoTokenizer
 
 # Download the file from Google Drive
 def download_file_from_google_drive(file_id, destination):
@@ -27,40 +14,48 @@ def download_file_from_google_drive(file_id, destination):
 
 # Set up the model paths
 MODEL_PATH = "./lawllm"
-DEST_FOLDER = "."
 
-if not os.path.exists(MODEL_PATH):
-    os.makedirs(MODEL_PATH)
-
-st.title("Contract Law AI Assistant")
-st.write("Ask any question about contract law:")
-
-if not st.session_state.model_loaded:
-    # Download each file
-    st.write("Downloading model files...")
-    download_file_from_google_drive("1qcqD5YRpTRt60iHaJCfSEqOR-xGdNEPu", os.path.join(MODEL_PATH, "config.json"))
-    download_file_from_google_drive("1Wdv0J1zAx20e2uzs6E3Ph9g8nA_Jf8Wk", os.path.join(MODEL_PATH, "pytorch_model.bin"))
-    download_file_from_google_drive("1vltJDB2dWuHz_oaNui-kqFzG14nRaNaL", os.path.join(MODEL_PATH, "vocab.json"))
-    download_file_from_google_drive("1wIltr1eGTQhmpNf9OU4lp-OCIqINbGFx", os.path.join(MODEL_PATH, "merges.txt"))
-    st.write("Model files downloaded!")
+@st.cache(allow_output_mutation=True, suppress_st_warning=True)
+def load_model():
+    # Check if model files exist
+    if not os.path.exists(os.path.join(MODEL_PATH, "config.json")):
+        # Create directory if it doesn't exist
+        if not os.path.exists(MODEL_PATH):
+            os.makedirs(MODEL_PATH)
+        
+        # Download each file
+        st.write("Downloading model files...")
+        download_file_from_google_drive("1qcqD5YRpTRt60iHaJCfSEqOR-xGdNEPu", os.path.join(MODEL_PATH, "config.json"))
+        download_file_from_google_drive("1Wdv0J1zAx20e2uzs6E3Ph9g8nA_Jf8Wk", os.path.join(MODEL_PATH, "pytorch_model.bin"))
+        download_file_from_google_drive("1vltJDB2dWuHz_oaNui-kqFzG14nRaNaL", os.path.join(MODEL_PATH, "vocab.json"))
+        download_file_from_google_drive("1wIltr1eGTQhmpNf9OU4lp-OCIqINbGFx", os.path.join(MODEL_PATH, "merges.txt"))
+        st.write("Model files downloaded!")
 
     # Load the model and tokenizer
     tokenizer = AutoTokenizer.from_pretrained(MODEL_PATH)
     model = AutoModelForCausalLM.from_pretrained(MODEL_PATH)
-    st.session_state.model_loaded = True
-else:
-    tokenizer = AutoTokenizer.from_pretrained(MODEL_PATH)
-    model = AutoModelForCausalLM.from_pretrained(MODEL_PATH)
-
-# Text input for the user's question
-user_input = st.text_input("Your Question:")
-
-if user_input and st.session_state.model_loaded:
-    with st.spinner('Generating response...'):
-        # Tokenize the user input and get model's answer
-        input_ids = tokenizer.encode(user_input, return_tensors="pt")
-        generated_ids = model.generate(input_ids, max_length=100, num_return_sequences=1)
-        answer = tokenizer.decode(generated_ids[0], skip_special_tokens=True)
     
-    # Display the model's answer
-    st.write("Answer:", answer)
+    return model, tokenizer
+
+def main():
+    st.title("Contract Law AI Assistant")
+    st.write("Ask any question about contract law:")
+    
+    # Load the model and tokenizer
+    model, tokenizer = load_model()
+
+    # Text input for the user's question
+    user_input = st.text_input("Your Question:")
+
+    if user_input:
+        with st.spinner('Generating response...'):
+            # Tokenize the user input and get model's answer
+            input_ids = tokenizer.encode(user_input, return_tensors="pt")
+            generated_ids = model.generate(input_ids, max_length=100, num_return_sequences=1)
+            answer = tokenizer.decode(generated_ids[0], skip_special_tokens=True)
+    
+        # Display the model's answer
+        st.write("Answer:", answer)
+
+if __name__ == "__main__":
+    main()
